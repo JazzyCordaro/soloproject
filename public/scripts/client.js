8604f8f73b370c1f8a43aa8c3d444704dc94a950
Jazzy Cordaro <jazzy_cordaro@hotmail.com>
2016-11-18 18:50:22 -0600

    another commit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,12 @@
 console.log('scripts is sourced!');
+
+// var searchURL = 'http://www.omdbapi.com/?s=' + searchNewMovie;
+//   $.ajax({
+//     url: searchURL,
+//     dataType: "JSON",
+//     success: function(data){
+//     console.log('successful API hit:', data);
+//     showMovies(data.Search);
+//     }
+//   }); // end ajax call
+// }); // end search new click
--- server/apps.js
+++ server/apps.js
@@ -18,4 +18,26 @@ app.get( '/', function( req, res ){
   res.sendFile( path.resolve( 'public/views/index.html' ) );
 }); // end base url
 
+// // talking to DB
+// app.get('/getMovies', function(req, res){
+//   console.log('getMovies route hit');
+//   //connect to DB: The Archive
+//   pg.connect(connectionString, function(err, client, done){
+//     if(err){
+//       console.log(err);
+//     }else{
+//       console.log('connected to DB');
+//       var resultsArray = [];
+//       var queryResults = client.query('SELECT * FROM movies ORDER BY title ASC');
+//       queryResults.on('row', function(row){
+//         resultsArray.push(row);
+//       });
+//       queryResults.on('end', function(){
+//         done();
+//         return res.json(resultsArray);
+//       }); // end queryResults
+//     } // end else
+//   });//end connect
+// }); // end /getMovies
+
 app.use(express.static('public'));
